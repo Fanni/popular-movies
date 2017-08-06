9339fff426d9d2e7f2ad2d1e83381248f7bd0240
Fanni <fannisik@gmail.com>
2017-08-06 21:41:34 +0700

    add data class handler and content provider

---
+++ app/src/main/java/com/project/udacity/popmov/MovieContentProvider.java
@@ -0,0 +1,136 @@
+package com.project.udacity.popmov;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import static com.project.udacity.popmov.MovieContract.MovieEntry.TABLE_NAME;
+
+/**
+ * Created by ikhwan on 8/6/17.
+ */
+
+public class MovieContentProvider extends ContentProvider {
+
+    public static final int MOVIES = 100;
+    public static final int MOVIES_WITH_ID = 101;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    private MovieDBHelper movieDBHelper;
+
+    public static UriMatcher buildUriMatcher() {
+
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI(MovieContract.AUTHORITY, MovieContract.PATH_MOVIES, MOVIES);
+        uriMatcher.addURI(MovieContract.AUTHORITY, MovieContract.PATH_MOVIES + "/#", MOVIES_WITH_ID);
+
+        return uriMatcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+
+        Context context = getContext();
+        movieDBHelper = new MovieDBHelper(context);
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+
+        final SQLiteDatabase sqLiteDatabase = movieDBHelper.getReadableDatabase();
+
+        int match = sUriMatcher.match(uri);
+        Cursor cursor;
+
+        switch (match) {
+            case MOVIES:
+                cursor = sqLiteDatabase.query(TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+
+        final SQLiteDatabase sqLiteDatabase = movieDBHelper.getWritableDatabase();
+
+        int match = sUriMatcher.match(uri);
+        Uri returnUri;
+
+        switch (match) {
+            case MOVIES:
+                long id = sqLiteDatabase.insert(TABLE_NAME, null, values);
+                if (id > 0) {
+                    returnUri = ContentUris.withAppendedId(MovieContract.MovieEntry.CONTENT_URI, id);
+                } else {
+                    throw new android.database.SQLException("Failed to insert data into " + uri);
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return returnUri;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+
+        final SQLiteDatabase sqLiteDatabase = movieDBHelper.getWritableDatabase();
+
+        int match = sUriMatcher.match(uri);
+
+        int moviesDeleted;
+
+        switch (match) {
+            case MOVIES_WITH_ID:
+                String id = uri.getPathSegments().get(1);
+                moviesDeleted = sqLiteDatabase.delete(TABLE_NAME, "movie_id=?", new String[]{id});
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        if (moviesDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return moviesDeleted;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ app/src/main/java/com/project/udacity/popmov/MovieContract.java
@@ -0,0 +1,42 @@
+package com.project.udacity.popmov;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by ikhwan on 8/6/17.
+ */
+
+public class MovieContract {
+
+    public static final String AUTHORITY = "com.project.udacity.popmov";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    public static final String PATH_MOVIES = "movies";
+
+    public static final class MovieEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI
+                .buildUpon()
+                .appendPath(PATH_MOVIES)
+                .build();
+
+        public static final String TABLE_NAME = "movies";
+
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+
+        public static final String COLUMN_TITLE = "title";
+
+        public static final String COLUMN_POSTER_PATH = "poster_path";
+
+        public static final String COLUMN_OVERVIEW = "overview";
+
+        public static final String COLUMN_VOTE = "vote";
+
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+
+        public static final String COLUMN_BACKDROP_IMAGE_PATH = "backdrop_image_path";
+
+    }
+}
